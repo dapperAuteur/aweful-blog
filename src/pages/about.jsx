2cb560d44e6055cd08a8e29bfcad7d709ce482c3
Anthony McDonald <a@awews.com>
2019-06-13 14:09:48 -0700

    add Layout to about.jsx component

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -15,28 +15,30 @@ const About = props => {
   const { title, date, tags } = frontmatter
 
   return (
-    <div key={id}>
-      {/* AboutMe Head */}
-      <Helmet title={`Blog - ${title}`} />
+    <Layout {...props}>
+      <div key={id}>
+        {/* AboutMe Head */}
+        <Helmet title={`Blog - ${title}`} />
 
-      <article className="blog-post">
-        {/* Title */}
-        <h1 className="title">{title}</h1>
-        {/* Date */}
-        <h2 className="date">{date}</h2>
-        {/* Back Button */}
-        <div className="backBtn">
-          <DefaultButton to={'/posts'}>{'Back'}</DefaultButton>
-        </div>
-        {/* Contents */}
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        {/* Post Tags */}
-        <TagButton list={tags || []} />
-      </article>
-    </div>
+        <article className="blog-post">
+          {/* Title */}
+          <h1 className="title">{title}</h1>
+          {/* Date */}
+          <h2 className="date">{date}</h2>
+          {/* Back Button */}
+          <div className="backBtn">
+            <DefaultButton to={'/posts'}>{'Back'}</DefaultButton>
+          </div>
+          {/* Contents */}
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+          {/* Post Tags */}
+          <TagButton list={tags || []} />
+        </article>
+      </div>
+    </Layout>
   )
 }
 
